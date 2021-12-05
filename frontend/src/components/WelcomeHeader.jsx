c29586966a7d09220ebc87b3451cc08c3da0fc8a
David Chavez <depch47@gmail.com>
2021-12-04 19:25:31 -0500

    navbar

--- frontend/src/components/WelcomeHeader.jsx
+++ frontend/src/components/WelcomeHeader.jsx
@@ -2,11 +2,11 @@ import { Button, Grid, Typography } from '@mui/material'
 
 const WelcomeHeader = ({ setSignUp }) => {
   return (
-    <Grid container justifyContent='center' alignItems='center' sx={{ mt: 2 }}>
+    <Grid container justifyContent='center' alignItems='center' sx={{ mt: 1 }}>
       <Grid item xs={7} md={4}>
         <Typography variant='h4'>AppName</Typography>
       </Grid>
-      <Grid item xs={4} md={4}>
+      <Grid item xs={2} md={2}>
         <Button onClick={() => setSignUp(true)}>Sign Up</Button>
         <Button onClick={() => setSignUp(false)}>Login</Button>
       </Grid>
--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -1,17 +1,17 @@
-import { Container, Grid } from '@mui/material';
-import { useState } from 'react';
-import LoginForm from './forms/LoginForm';
-import SignUpForm from './forms/SignUpForm';
-import WelcomeHeader from './WelcomeHeader';
+import { Container } from '@mui/material'
+import { useState } from 'react'
+import LoginForm from './forms/LoginForm'
+import SignUpForm from './forms/SignUpForm'
+import WelcomeHeader from './WelcomeHeader'
 
 const WelcomePage = () => {
-  const [signUp, setSignUp] = useState(false);
+  const [signUp, setSignUp] = useState(false)
   return (
     <Container>
       <WelcomeHeader setSignUp={setSignUp} />
       {signUp ? <SignUpForm /> : <LoginForm />}
     </Container>
-  );
-};
+  )
+}
 
-export default WelcomePage;
+export default WelcomePage
