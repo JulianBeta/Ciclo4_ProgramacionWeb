15b0ac2981439c26ff4cf680f9e0d4b52e4681b0
David Chavez <depch47@gmail.com>
2021-12-04 19:23:46 -0500

    navbar

--- frontend/src/components/WelcomeHeader.jsx
+++ frontend/src/components/WelcomeHeader.jsx
@@ -1,10 +1,17 @@
+import { Button, Grid, Typography } from '@mui/material'
+
 const WelcomeHeader = ({ setSignUp }) => {
   return (
-    <div>
-      <button onClick={() => setSignUp(true)}>sign up</button>
-      <button onClick={() => setSignUp(false)}>login</button>
-    </div>
-  );
-};
+    <Grid container justifyContent='center' alignItems='center' sx={{ mt: 2 }}>
+      <Grid item xs={7} md={4}>
+        <Typography variant='h4'>AppName</Typography>
+      </Grid>
+      <Grid item xs={4} md={4}>
+        <Button onClick={() => setSignUp(true)}>Sign Up</Button>
+        <Button onClick={() => setSignUp(false)}>Login</Button>
+      </Grid>
+    </Grid>
+  )
+}
 
-export default WelcomeHeader;
+export default WelcomeHeader
